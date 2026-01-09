1a0f4473a12913609cb00060bfdc2ff1b101e899
Nnanyelugo Tech <nnanyelugomeputaife@gmail.com>
2026-01-09 21:08:28 +0100

    deleted some fills

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-// import { Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Admin from "./pages/Admin";
-// import Jobs from "./pages/Jobs";
-
-// export default function App() {
-//   return (
-//     <div className="min-h-screen flex flex-col">
-//       <Header />
-
-//       <main className="flex-1">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/jobs" element={<Jobs />} />
-//           <Route path="/admin" element={<Admin />} />
-//         </Routes>
-//       </main>
-
-//       <Footer />
-//     </div>
-//   );
-// }
-
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import Jobs from "./pages/JobsPage";
-import Admin from "./pages/Admin";
 
 export default function App() {
   return (
@@ -35,10 +9,7 @@ export default function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/jobs" element={<Jobs />} />
-        <Route path="/admin" element={<Admin />} />
       </Route>
     </Routes>
   );
 }
-
-
